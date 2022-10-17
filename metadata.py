e2d2af57add1b1dee8331eea79990f515168a1b0
Linxzh <linxzh1989@gmail.com>
2022-10-17 21:00:20 +0800

    Update metadata.py

--- metadata.py
+++ metadata.py
@@ -25,9 +25,9 @@ with open("data/translators.json", 'w', encoding='utf-8') as handle:
     json.dump(translator_metadata, handle)
 
 
-try:
-    os.system("git add data/translators.json")
-    os.system("git commit -m 'Update metadata %s'" % datetime.now())
-    os.system("git push origin HEAD:master")
-except:
-    print("Error")
+# try:
+#     os.system("git add data/translators.json")
+#     os.system("git commit -m 'Update metadata %s'" % datetime.now())
+#     os.system("git push origin HEAD:master")
+# except:
+#     print("Error")
