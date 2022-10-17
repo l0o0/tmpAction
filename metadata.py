160d9de176716f4b2e29fe050290293b4c987790
Linxzh <linxzh1989@gmail.com>
2022-10-17 11:18:49 +0800

    add: 添加获取转换器元数据脚本

---
+++ metadata.py
@@ -0,0 +1,22 @@
+import json
+import os
+
+
+def read_metadata(filename):
+    with open(filename, encoding='utf-8') as handle:
+        headers = [next(handle) for x in range(13)]
+    return json.loads(''.join(headers))
+
+translators = os.listdir("translators")
+translators = [t for t in translators if t.endswith('js') and t != 'RefWorks Tagged.js']
+
+translator_metadata = {}
+for t in translators:
+    metadata = read_metadata("translators/" + t)
+    translator_metadata[t] = {
+        'label': metadata['label'],
+        'lastUpdated': metadata['lastUpdated']
+    }
+
+with open("data/translators.json", 'w', encoding='utf-8') as handle:
+    json.dump(translator_metadata, handle)
